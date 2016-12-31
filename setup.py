fb9032c405681125f2f93bc578df5b75351df135
Claude.Seo <ehdaudtj@gmail.com>
2016-12-31 15:03:12 +0900

    프로젝트명 변경

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ try:
 except ImportError:
     from distutils.core import setup
 from pip.req import parse_requirements
-from src.flask_generator import __version__
+from src.flask_app_generator import __version__
 
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session='hack')
 reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
-    name='flask_generator',
+    name='flask_app_generator',
     version=__version__,
-    packages=['flask_generator'],
-    package_dir={'flask_generator': 'src/flask_generator'},
+    packages=['flask-app-generator'],
+    package_dir={'flask-app-generator': 'src/flask-app-generator'},
     install_requires=reqs,
     author='Claude.Seo',
     author_email='ehdaudtj@gmail.com',
-    url='https://github.com/SeoDongMyeong/flask-generator',
+    url='https://github.com/SeoDongMyeong/flask-app-generator',
     description='Flask Project Generator',
     keywords=['flask', 'flask-project', 'flask-generator'],
     license='MIT License'
