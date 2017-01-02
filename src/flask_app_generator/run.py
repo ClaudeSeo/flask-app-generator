b3c49971bb93ef63875531019a509964c64f0733
Claude.Seo <ehdaudtj@gmail.com>
2017-01-02 15:53:21 +0900

    실행 스크립트 수정

--- src/flask_app_generator/run.py
+++ src/flask_app_generator/run.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from optparse import OptionParser
-from generator import AppGenerator
+from generator import AppGenerator, APP_TYPES
 
 
 def main():
@@ -10,7 +10,7 @@ def main():
         '''
     parser = OptionParser(usage=usage)
     parser.add_option('-t', '--type', dest='type',
-                      help='[Required] 1: Simple, 2: Large', type='int')
+                      help='[Required] Type: simple, large', type='string')
     parser.add_option('-n', '--name', dest='name', type='string',
                       help='[Required] Project name')
     (options, args) = parser.parse_args()
@@ -24,6 +24,15 @@ def main():
 
     assert app_type, parser.error('type is required')
     assert app_name, parser.error('name is required')
+
+    app_type = app_type.upper()
+
+    if app_type == 'SIMPLE':
+        app_type = APP_TYPES.SIMPLE
+    elif app_type == 'LARGE':
+        app_type = APP_TYPES.LARGE
+    else:
+        parser.error('type is not support %s' % app_type)
 
     gen = AppGenerator(app_type, app_name)
     gen.init_app()
